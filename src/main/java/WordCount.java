1015cd6a7365116a5a139baa8c1d6cb1306ec407
shanikaveti <95012951+shanikaveti@users.noreply.github.com>
2023-04-15 14:33:07 -0500

    Update WordCount.java

--- src/main/java/WordCount.java
+++ src/main/java/WordCount.java
@@ -1,16 +1,19 @@
-//package org.example;
-
 import java.util.HashMap;
 import java.util.Map;
 
 public class WordCount {
     public static void main(String[] args) {
-        String document = "I am doing software engineering";
+        String document = args[0];
+//        String document = "I am doing software engineering";
         Map<String, Integer> wordFrequency = getWordFrequency(document);
+        int lineCount = getLineCount(document);
+        int charCount = getCharCount(document);
         System.out.println("Word frequency:");
         for (Map.Entry<String, Integer> entry : wordFrequency.entrySet()) {
             System.out.println(entry.getKey() + ": " + entry.getValue());
         }
+        System.out.println("Line count: " + lineCount);
+        System.out.println("Character count: " + charCount);
     }
 
     public static Map<String, Integer> getWordFrequency(String document) {
@@ -22,5 +25,14 @@ public class WordCount {
             }
         }
         return frequency;
+    }
+
+    public static int getLineCount(String document) {
+        String[] lines = document.split("\\r?\\n");
+        return lines.length;
+    }
+
+    public static int getCharCount(String document) {
+        return document.length();
     }
 }
